20110126496374bc37bfdddd6e7d4be1ed778400
LucasDaine <150380297+LucasDaine@users.noreply.github.com>
2023-11-14 13:43:55 -0300

    Nos aseguramos que el usuario cargue los valores correctos

--- Proyecto-Mutantes-Lucas-Daine.py
+++ Proyecto-Mutantes-Lucas-Daine.py
@@ -3,9 +3,16 @@ print("M A G N E T O   C O R P S")
 while True:
     print("Ingrese su ADN (A,T,C,G) en mayúsculas")
     matriz = list(input())
-    if (len(matriz) != 36):
-        print ("los datos ingresados son incorrectos")
+    contador = 0
+    if (len(matriz) == 36):
+        for letra in matriz:
+            if (letra == "A" , "G" , "T" ,"C"):
+                contador = contador + 1
+    if (contador != 36):  
+        print("parece que has ingresado mal los datos, intenta de nuevo")
     else: break
 
+print("ANALIZANDO...")
 
-## matriz 6x6 #
+## matriz 6x6 
+# letras A - T - C - G #
