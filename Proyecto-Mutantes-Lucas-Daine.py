6c90df1cecfda149e28a5f138963ab694c87f109
LucasDaine <150380297+LucasDaine@users.noreply.github.com>
2023-11-15 22:01:03 -0300

    Charles Xavier me controló la mente y me hizo creer que podía resolver este trabajo sin crear una verdadera matriz

--- Proyecto-Mutantes-Lucas-Daine.py
+++ Proyecto-Mutantes-Lucas-Daine.py
@@ -1,7 +1,11 @@
+## matriz 6x6 
+# letras A - T - C - G 
+
 print("M A G N E T O   C O R P S\n")
 
 while True:
-    print("Ingrese su ADN (A,T,C,G) en mayúsculas")
+    print("Ingrese el ADN (A,T,C,G) en mayúsculas")
+    print("Recuerda que son 36 caracteres")
     matriz = list(input())
     contador = 0
     if (len(matriz) == 36):
@@ -9,46 +13,30 @@ while True:
             if (letra == "A" , "G" , "T" ,"C"):
                 contador = contador + 1
     if (contador != 36):  
-        print("parece que has ingresado mal los datos, intenta de nuevo")
+        print("parece que has ingresado mal los datos, intenta de nuevo\n")
     else: break
 
 print("ANALIZANDO...\n")
 
 print("su ADN es:\n")
 
-contFilas = 0
-contColum = 0
-for elemento in matriz:
-    contFilas = contFilas + 1
-    print("[",elemento,end=" ]")
-    if (contFilas > 5):
-        print("\n")
-        contFilas = 0
+##Charles Xavier me controló la mente y me hizo creer que podía resolver este
+# trabajo sin la necesidad de crear una verdadera matriz... me equivoqué#
 
 mutante = 0
 
-conteoH = 1
-for i in range (0,5):
-    for j in range (0,5):
-        if (matriz[j] == matriz[j+1]):
-            conteoH = conteoH+1
-            if (conteoH == 4):
-                print("Es Mutante")
-                break
-            else: mutante = 0
+def listas(matriz):
+    for i in range (0,6):
+        i = list()
+        for j in range (0,6):
+            i.append(matriz[j])
+        print(i)
 
-conteoV = 1
-for i in range (0,5):
-    for j in range (0,5):
-        if (matriz[i] == matriz[i+6]):
-            conteoV = conteoV+1
-            if (conteoV == 4):
-                print("Es Mutante")
-                break
-            else: mutante = 0
+listas(matriz)
 
+##le confirmo el resultado al usuario
+if (mutante==1):
+    print("Es Mutante")
+else: print("No es Mutante")
 
-
-## matriz 6x6 
-# letras A - T - C - G 
-# ATCGTCATCGCTACGTCGACGTCGATGCTCATGCTC#
+# ATCGATCGATCGATCGATCGATCGATCGATCGAAAA#
